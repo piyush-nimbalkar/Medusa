5bc40e0ea2fc2f1c6c5fdfb0f47781a91bbdf422
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2013-12-02 16:23:11 -0500

    [PB/PN] Changed how we merge dicts and return key-value pair from most_frequent function

--- frequency.erl
+++ frequency.erl
@@ -2,27 +2,48 @@
 -export([start/0]).
 
 start() ->
-        Packet = "...Heytretrrrrrrrrrrrrrrr$%$#?? you good you you terminal, Happy Thanksgiving!",
-	Packet2 = "good terminal when is Thanksgiving!",
-	Dict1 = find_frequency(Packet),
-	Dict2 = find_frequency(Packet2),
-	D3 = merge(Dict1,Dict2),
-	print_dict(D3).
+    Packet = "...Heytretrrrrrrrrrrrrrrr$%$#?? you good you you terminal, Happy Thanksgiving!",
+    Packet2 = "good terminal when is Thanksgiving!",
+    Dict1 = create_dictionary(Packet),
+    Dict2 = create_dictionary(Packet2),
+    MergedDict = merge(Dict1, Dict2),
+    print_dict(Dict1),
+    print_dict(Dict2),
+    print_dict(MergedDict),
+    find_most_frequent(MergedDict).
 
-merge(D1,D2) ->
-	D3 = dict:merge(fun(K,V1,V2) -> V1+V2 end , D1,D2).
 
-find_frequency(Packet) ->
-	Tokens = string:tokens(Packet, " "),
-	Dict = lists:foldl(
-                        fun(W, D) -> 
-       				{match, [{Start, Length}]} = re:run(W, "[a-zA-Z]+"),
-        			Word = string:substr(W, Start+1, Length),
-				dict:update(Word, fun(C) -> C + 1 end, 1, D)
-				 end, 
-                        dict:new(), 
-                        Tokens).
+merge(Dict1, Dict2) ->
+    dict:fold(fun(K2, V2, D1) ->
+                      case (dict:find(K2, D1)) of
+                          {ok, _} -> dict:update(K2, fun(V1) -> V1 + V2 end, D1);
+                          error -> D1
+                      end
+              end, Dict1, Dict2).
+
+
+create_dictionary(Packet) ->
+    Tokens = string:tokens(Packet, " "),
+    lists:foldl(fun(Word, Dict) ->
+                        {match, [{Start, Length}]} = re:run(Word, "[a-zA-Z]+"),
+                        RefinedWord = string:substr(Word, Start+1, Length),
+                        dict:update(RefinedWord, fun(Count) -> Count + 1 end, 1, Dict)
+                end, dict:new(), Tokens).
+
+
+find_most_frequent(Dict) ->
+    {MaxKey, MaxValue} = dict:fold(fun(K, V, {MaxKey, MaxValue}) ->
+                                           case (V > MaxValue) of
+                                               true -> {K, V};
+                                               false -> {MaxKey, MaxValue}
+                                           end
+                                   end, {non_existing_key, 0}, Dict),
+    io:format("Maximum = ~s : ~B~n", [MaxKey, MaxValue]).
+
 
 print_dict(Dict) ->
-    dict:fold(fun(Word, Count, AccIn) -> 
-		io:format("~s: ~w~n", [Word, Count]), AccIn end, void, Dict).
+    dict:fold(fun(Word, Count, AccIn) ->
+                      io:format("~s: ~w~n", [Word, Count]),
+                      AccIn
+              end, void, Dict),
+    io:format("------------------~n").
