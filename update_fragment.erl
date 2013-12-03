d602e1c48dfe0512eddde9c9f5b4151d0de322ec
padma <padma1@umbc.edu>
2013-12-03 14:38:53 -0500

    Update word in fragment

--- update_fragment.erl
+++ update_fragment.erl
@@ -5,26 +5,27 @@ start() ->
         Packet = "...Heytretrrrrrrrrrrrrrrr$%$#?? you good terminal, Happy Thanksgiving!",
         Old_Word = "Thanksgiving",
         New_Word = "Christmas",
-	find_word(Packet, Old_Word,New_Word).
+	update_word( Packet, Old_Word, New_Word).
 
-find_word(Packet, Old_Word, New_Word) ->
+update_word( Packet, Old_Word, New_Word) ->
         Tokens = string:tokens(Packet, " "),
-        Result = search_word(Tokens, Old_Word,New_Word),
-        io:format("Result : ~s~n",[Result]).
+        ResultList = search_word(Tokens, Old_Word,New_Word),
+        ResultFrag = string:join( ResultList, " "),
+%        io:format("RES ::: ~p~n", [ResultFrag]),
+        ResultFrag.
 
-search_word(Tokens, Old_Word, New_Word) when length(Tokens) == 0 ->
-        false;
+
+search_word( Tokens, Old_Word, New_Word) when length(Tokens) == 0 ->
+        [];
 
 search_word( Tokens, Old_Word, New_Word) ->
         [Head | Tail] = Tokens,
         {match, [{Start, Length}]} = re:run(Head, "[a-zA-Z]+"),
         Token = string:substr(Head, Start+1, Length),
         if Token == Old_Word ->
-		io:format("~p~n",[Head]),
-%		Head = New_Word,
-                Head;
+                List = [New_Word] ++ search_word( Tail, Old_Word,New_Word);
         true ->
-                Result = search_word( Tail, Old_Word,New_Word),
-                Result
-        end.
+                List = [Head] ++ search_word( Tail, Old_Word,New_Word)
+        end,
+        List.
 
